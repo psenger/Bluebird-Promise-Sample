dac3953b9712570c6782a5ad7070355f154d4549
Philip A Senger <philip.a.senger@cngrgroup.com>
2018-01-15 10:20:39 +1100

    Scoped the work. Looks a little better now

--- promises_flow_control.js
+++ promises_flow_control.js
@@ -7,21 +7,19 @@
 
 const Promise = require ( 'bluebird' );
 
-let count = 0;
-
 /**
  * Promises generate while.. pushing to the process.nextTick
- * @param condition
- * @param action
+ * @param {function} condition - This function will be passed the work, returning true keeps the action going.
+ * @param {function} action - some discrete piece of work to perform on the work.
+ * @param {Object} work - an Object that is passed around, I was thinking of binding it, but it is not needed.
  */
-const promiseWhile = (condition, action) => {
+const promiseWhile = ( condition, action, work ) => {
     return new Promise( (resolve, reject) => {
-                            const results = [];
                             const loop = () => {
-                                if (!condition()) {
-                                    return resolve( results );
+                                if (!condition( work )) {
+                                    return resolve( work );
                                 }
-                                return Promise.cast(action( results )) // A way of casting incoming then-ables or promise subclasses to promises of the exact class specified, so that the resulting object's then is ensured to have the behavior of the constructor you are calling cast on.
+                                return Promise.cast(action( work )) // A way of casting incoming then-ables or promise subclasses to promises of the exact class specified, so that the resulting object's then is ensured to have the behavior of the constructor you are calling cast on.
                                     .then(loop)
                                     .catch(function (e) {
                                         reject(e);
@@ -33,29 +31,37 @@ const promiseWhile = (condition, action) => {
 
 /**
  * This is a predicate function which shall return true or false and indicate whether the end state has been reached. true if done, false otherwise.
- * @returns {boolean}
+ * @param {Object} work - an Object that is passed around. Holds all the work :smile:
+ * @returns {boolean} - true indicates that the work should continue.
  */
-const conditionFn = () => {
-    console.log(`\tchecking if count <= 5 ... ${(count <= 5)}`);
-    return count < 5;
+const conditionFn = ( work ) => {
+    console.log(`\tchecking if count <= 5 ... ${( work.count <= 5)}`);
+    return work.count < 5;
 };
 
 /**
  * This is the async task to be performed, which itself should return a Promise.
+ * @param {Object} work - an Object that is passed around.
+ * @returns {*|PromiseLike<T>|Promise<T>}
  */
-const workFn = ( results ) => {
-    console.log(`Worker function called, last result was = ${results}`);
+const workFn = ( work ) => {
+    console.log(`Worker function called, last result was = ${JSON.stringify(work)}`);
     return new Promise
                 .delay(500) // here for demo purposes only.
                 .then(()=>{
-                    count++;
-                    results.push( count );
-                    console.log(`\they I woke up and count is now ${count}`);
+                    work.count++;
+                    ! work.results && ( work.results = [] );
+                    work.results.push( work.count );
+                    console.log(`\they I woke up and count is now ${work.count}`);
                     return;
                 });
-}
+};
 
-Promise.join( promiseWhile(conditionFn, workFn) )
+let work = {
+    count: 0
+};
+
+Promise.join( promiseWhile( conditionFn, workFn, work ) )
         .then(function ( result ) {
             console.log(`result = ${JSON.stringify(result,'\n',4)}`);
         }).catch(function (e) {
